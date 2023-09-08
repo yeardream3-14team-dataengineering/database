1e99dfd45183f6c6891db7311366160d1c16c04b
humaningansalam <sanghun9634@gmail.com>
2023-09-08 17:17:51 +0900

    fetch: add mysql v1.4 sql file

---
+++ mysql/v1.4/team14_생성쿼리_ver1.4.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS mydb_test;
+USE mydb_test;
+
+CREATE TABLE IF NOT EXISTS Category
+(
+	category_id INT NOT NULL PRIMARY KEY
+    , category_name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Article
+(
+	news_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+    , category_id INT NOT NULL
+    , news_url VARCHAR(300) NOT NULL UNIQUE
+    , company_name VARCHAR(20) NOT NULL
+    , title VARCHAR(300) NOT NULL
+    , author_info VARCHAR(300) NOT NULL
+    , create_date DATETIME NOT NULL
+    , modify_date DATETIME NULL
+    , content LONGTEXT NOT NULL
+    , image_url VARCHAR(1000) NULL
+    , comments INT NULL DEFAULT 0
+    , gendor_ratio TINYINT NULL DEFAULT 0
+    , age_ratio TINYINT NULL DEFAULT 0
+    , FOREIGN KEY (category_id) REFERENCES Category (category_id)
+);
+
+insert into Category (category_id, category_name)
+values (100, "정치"), (101, "경제"), (102, "사회"), (103, "생활/문화"), (104, "세계"), (105, "IT/과학");
